e0c543552d421536f4492a76a692d067b6418565
Anthony Messana <messana1@tcnj.edu>
2025-06-13 01:21:01 -0400

    add separator method and keyword support

--- Lexer.scala
+++ Lexer.scala
@@ -1,20 +1,36 @@
 import java.io.RandomAccessFile
 import scala.collection.immutable.*
 import scala.Option
+import scala.compiletime.ops.float
+import scala.compiletime.ops.string
 
 enum Token:
     case Keyword(value: String)
     case Identifier(value: String)
     case Literal(value: String)
     case Operator(value: String)
-    case Seperator(value: String)
+    case Separator(value: String)
     case Comment(value: String)
     case Invalid
     case EOF
-
+/* 
+TODO: 
+    Use peek + skipChar instead of nextChar, as nextChar causes separators at the end of literals 
+    and identifiers to be skipped in the subsequent call to nextToken.
+*/
 class Lexer(filename: String):
     val source = RandomAccessFile(filename, "r")
-    private def nextChar = if source.getFilePointer < source.length then Some(source.read().toChar) else None
+    def index = source.getFilePointer
+    def skipChar = source.seek(index + 1)
+    def ERROR(message: String = "") = 
+        if message.nonEmpty then println(message)
+        (Token.Invalid, index)
+
+    private def nextChar = if index < source.length then Some(source.read().toChar) else None
+    private def peek = 
+        nextChar match
+            case Some(value) => source.seek(index - 1); Some(value)
+            case None => None
     
     private val isDec: (Char => Boolean) = _.isDigit
     private def isOctal(c: Char) = 0 <= '7' - c && '7' - c <= 7
@@ -26,45 +42,72 @@ class Lexer(filename: String):
         c == '=' || c == '^'
     // these are operators that do not form another operator if repeated twice
     private def isSingleOp(c: Char) = c == '!' || c == '^' || c == '*' || c == '/' || c == '%'
+    
     private def isEscapeChar(c: Char) = 
         c == 't' || c == 'b' || c == 'n' || c == 'r' ||
         c == 'f' || c == '\'' || c == '"' || c == '\\' 
+    
+    private def isSeparator(c: Char) = 
+        c == '{' || c == '}' || c == ';' || c == ',' ||
+        c == '(' || c == ')' || c == '[' || c == ']' ||
+        c == '.' || c == ':' || c == '@' 
+
+
+    val keywords: Set[String] = Set(
+    "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
+    "continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float",
+    "for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long",
+    "native", "new", "package", "private", "protected", "public", "return", "short", "static",
+    "strictfp", "super", "switch", "synchronized", "this", "throw", "throws", "transient",
+    "try", "void", "volatile", "while"
+    )
 
     def identifier(lexeme: String): (Token, Long) =
         nextChar match
             case Some(c) if c.isLetterOrDigit || c == '_' || c == '$' => identifier(lexeme + c)
-            case Some(c) if c.isWhitespace => (Token.Identifier(lexeme), source.getFilePointer)
+            case Some(c) if isSeparator(c) || c.isWhitespace => (Token.Identifier(lexeme), index)
             case Some(e) => 
-                println(s"ERROR: Identifier contains illegal character $e")
-                (Token.Identifier(lexeme), source.getFilePointer)
-            case None => (Token.Identifier(lexeme), source.getFilePointer)
+                ERROR("Identifier contains illegal character $e")
+            case None => (Token.Identifier(lexeme), index)
 
     def intLiteral(lexeme: String)(f: Char => Boolean): (Token, Long) = 
         nextChar match
             case Some(c) if f(c) => intLiteral(lexeme + c)(f)
-            case Some(c) if c.isLetterOrDigit => (Token.Invalid, source.getFilePointer)
-            case Some(c) if c == '.'  => 
-                if f == isDec then floatLiteral(lexeme + c) 
-                else (Token.Invalid, source.getFilePointer)
-            case Some(_) => (Token.Literal(lexeme), source.getFilePointer)
-            case None => (Token.Literal(lexeme), source.getFilePointer)
+            case Some('l') | Some('L') => skipChar; (Token.Literal(lexeme), index)
+            case Some(c) if c.isLetterOrDigit => ERROR()
+            case Some('_') =>
+                peek match
+                    case Some(c) if c.isDigit => intLiteral(lexeme)(f)
+                    case Some(_) | None => ERROR() 
+            case Some('.') => if f == isDec then floatLiteral(lexeme + '.') else ERROR()
+            case Some(c) if isSeparator(c) || isOperator(c) || c.isWhitespace => (Token.Literal(lexeme), index)
+            case Some(_) => ERROR()
+            case None if lexeme.nonEmpty => (Token.Literal(lexeme), index)
+            case None => ERROR()
+
 
     def floatLiteral(lexeme: String): (Token, Long) =
         nextChar match
             case Some(c) if c.isDigit => floatLiteral(lexeme + c)
-            case Some('F') | Some('f') => (Token.Literal(lexeme), source.getFilePointer)
-            case Some('d') => (Token.Literal(lexeme), source.getFilePointer)
-            case Some(c) if c.isLetter => (Token.Invalid, source.getFilePointer)
-            case Some(_) => (Token.Literal(lexeme), source.getFilePointer)
-            case None if lexeme.charAt(lexeme.length-1) == '.' => (Token.Invalid, source.getFilePointer)
-            case None => (Token.Literal(lexeme), source.getFilePointer)
+            case Some('F') | Some('f') => skipChar; (Token.Literal(lexeme), index)
+            case Some('d') => (Token.Literal(lexeme), index)
+            case Some('.') => ERROR()
+            case Some(c) if c.isLetter => ERROR()
+            case Some(_) => (Token.Literal(lexeme), index)
+            case None if lexeme.charAt(lexeme.length-1) == '.' => ERROR()
+            case None => (Token.Literal(lexeme), index)
 
     def stringLiteral(lexeme: String): (Token, Long) = 
-        (lexeme, nextChar) match
-            case (_, Some(c)) if c == '"' => (Token.Literal(lexeme), source.getFilePointer)
-            case (s"$l\\", Some(c)) if isEscapeChar(c) => stringLiteral((lexeme + c).translateEscapes)
-            case (_, Some(c)) => stringLiteral(lexeme + c)
-            case (_, None) => println("ERROR: Unclosed string literal"); (Token.Invalid, source.getFilePointer)
+        nextChar match
+            case Some('"') => (Token.Literal(lexeme), index)
+            case Some('\\') => 
+                nextChar match
+                    case Some(c) => 
+                        if isEscapeChar(c) then stringLiteral(lexeme + ("\\" + c).translateEscapes)
+                        else stringLiteral(lexeme + c)
+                    case None => ERROR("Unclosed string literal")
+            case Some(c) => stringLiteral(lexeme + c)
+            case None => ERROR("Unclosed string literal")
  
     def operator(lexeme: String): (Token, Long) =
         nextChar match
@@ -72,32 +115,42 @@ class Lexer(filename: String):
                 if lexeme.length == 1 && isOperator(c) && c == '=' || lexeme.charAt(0) == c && !isSingleOp(c)
                 => operator(lexeme + c)
             case Some('=') if lexeme == ">>" || lexeme == "<<" => operator(lexeme + '=')
-            case Some(c) if !isOperator(c) => (Token.Operator(lexeme), source.getFilePointer)
-            case Some(_) => (Token.Invalid, source.getFilePointer)
-            case None => (Token.Operator(lexeme), source.getFilePointer)
+            case Some(c) if !isOperator(c) => (Token.Operator(lexeme), index)
+            case Some(_) => ERROR()
+            case None => (Token.Operator(lexeme), index)
 
     def nextToken(pos: Long): (Token, Long) = 
         source.seek(pos)
+        println(source.length)
+        println(s"Getting next token starting from positon $pos")
         nextChar match
             case Some(c) =>
                 val curLexeme = c.toString
                 if c.isWhitespace then
                     nextToken(pos + 1) 
                 else if c.isLetter || c == '_' || c == '$' then 
-                    identifier(curLexeme)
+                    identifier(curLexeme) match
+                        case (Token.Identifier(id), i) if keywords contains id => (Token.Keyword(id), i)
+                        case t => t
+                else if c == '0' then
+                    peek match
+                        case Some('b') => skipChar; intLiteral("")(isBin)
+                        case Some('x') => skipChar; intLiteral("")(isHex)
+                        case Some(la) if la.isDigit => intLiteral(curLexeme + la)(isOctal)
+                        case _ => intLiteral(curLexeme)(isDec)
                 else if c.isDigit then
-                    (c, nextChar) match 
-                        case ('0', Some(la)) if la == 'x' => intLiteral(curLexeme + la)(isHex)
-                        case ('0', Some(la)) if la == 'b' => intLiteral(curLexeme + la)(isBin)
-                        case ('0', Some(la)) if la.isDigit => intLiteral(curLexeme + la)(isOctal)
-                        case (_, Some(la)) if la.isDigit => intLiteral(curLexeme + la)(isDec)
-                        case (_, Some(la)) if la.isWhitespace => (Token.Literal(curLexeme), source.getFilePointer)
-                        case (_, None) => (Token.Literal(curLexeme), source.getFilePointer)
-                        case (_, Some(_)) => println("ERROR: Malformed Int literal"); (Token.Invalid, source.getFilePointer)
+                    intLiteral(curLexeme)(isDec)
+                else if c == '.' then
+                    floatLiteral(curLexeme)
                 else if c == '"' then
                     stringLiteral(curLexeme)
                 else if isOperator(c) then
                     operator(curLexeme)
+                else if isSeparator(c) then
+                    println("SEPARATOR")
+                    (c, peek) match
+                        case (':', Some(':')) => skipChar; (Token.Separator("::"), index)
+                        case _ => (Token.Separator(curLexeme), index)
                 else
-                    println("ERROR: Unrecognized lexeme"); (Token.Invalid, source.getFilePointer())
-            case None => println("ERROR: EOF"); (Token.EOF, source.length)
+                    ERROR("Unrecognized lexeme")
+            case None => println("ERROR(): EOF"); (Token.EOF, source.length)
